b5ef764deb6bd4f4573307ec3c89b2b18441cca0
hqDkkk <1636891043@qq.com>
2025-02-20 23:50:59 +0800

    about urdf

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros1/work2025/src
+# Build directory: /home/ros1/work2025/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
